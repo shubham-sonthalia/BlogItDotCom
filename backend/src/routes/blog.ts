78ec32f1b1e451eae8b37f4e74778d5288313709
Shubham Sonthalia <shubhamsonthalia@Shubhams-MacBook-Air.local>
2024-03-16 15:11:32 +0530

    all backend routes created

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,10 +1,17 @@
 import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 import { decode, jwt, sign, verify } from "hono/jwt";
 
 type Variables = {
   userId: string;
   prisma: any;
 };
+
+const BlogPostData = z.object({
+  title: z.string().max(100),
+  content: z.string(),
+});
 export const blog = new Hono<{
   Bindings: {
     JWT_SECRET: string;
@@ -36,16 +43,49 @@ blog.use("*", async (c, next) => {
   }
 });
 
-blog.post("", (c) => {
-  console.log(c.get("userId"));
-  return c.text("sign in route");
+blog.post("", zValidator("json", BlogPostData), async (c) => {
+  try {
+    const body = await c.req.json();
+    const prisma = c.get("prisma");
+    const userId = c.get("userId");
+    const res = await prisma.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: userId,
+      },
+    });
+    return c.json(res);
+  } catch (error) {
+    console.log(error);
+    c.status(500);
+    return c.json({ error: "Error in creating new post" });
+  }
 });
 
-blog.put("", (c) => {
-  console.log(c.get("userId"));
-  return c.text("Hello Hono!");
+blog.put("", async (c) => {
+  const body = await c.req.json();
+  const prisma = c.get("prisma");
+  const userId = c.get("userId");
+  const updatePost = await prisma.post.update({
+    where: {
+      id: body.id,
+    },
+    data: {
+      title: body.title,
+      content: body.content,
+    },
+  });
+  return c.json({ msg: "Post Updated successfully!", result: updatePost });
 });
 
-blog.get(":id", (c) => {
-  return c.text("Hello Hono!");
+blog.get(":id", async (c) => {
+  const id = parseInt(c.req.param("id"));
+  const prisma = c.get("prisma");
+  const post = await prisma.post.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  return c.json(post);
 });
